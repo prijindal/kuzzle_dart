56c52ad8dd82b0dae04f6a0492df0538cca45ec0
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2020-04-02 13:50:38 +0000

    Fixed coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 mkdir coverage
-dart --pause-isolates-on-exit --enable_asserts --enable-vm-service test/.test_coverage.dart &
+dart --pause-isolates-on-exit --enable_asserts --enable-vm-service test/.test_coverage.dart > dart-output.txt &
 sleep 30
-pub run coverage:collect_coverage --uri=http://127.0.0.1:8181 -o coverage/coverage.json --resume-isolates
+observatory_line=`grep "Observatory listening on http://127.0.0.1:" dart-output.txt`
+uri_value=${observatory_line#*Observatory listening on }
+echo $uri_value
+pub run coverage:collect_coverage --uri="$uri_value" -o coverage/coverage.json --resume-isolates
 pub run coverage:format_coverage -l --packages=$(pwd)/.packages -i coverage/coverage.json --report-on=lib -o coverage/lcov.info
 curl -s https://codecov.io/bash > coverage/.codecov
 sed -i -e 's/TRAVIS_.*_VERSION/^TRAVIS_.*_VERSION=/' coverage/.codecov
